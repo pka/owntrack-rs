5bd474167e18a8354a9cdf103faa48bf09b39380
Pirmin Kalberer <pirmin.kalberer@gmail.com>
2025-06-15 22:51:41 +0200

    Show pulsating dot for moving devices

--- frontend/src/routes/Map.svelte
+++ frontend/src/routes/Map.svelte
@@ -10,6 +10,7 @@
         CircleLayer,
         SymbolLayer,
         Popup,
+        Image,
     } from "svelte-maplibre-gl";
     import maplibregl from "maplibre-gl";
     const { LngLatBounds } = maplibregl;
@@ -58,6 +59,57 @@
         }
         return str;
     }
+
+    const pulseSize = 170;
+
+    // Implement `StyleImageInterface` to draw a pulsing dot icon.
+    const pulsingDot = {
+        width: pulseSize,
+        height: pulseSize,
+        data: new Uint8Array(pulseSize * pulseSize * 4),
+
+        onAdd: function () {
+            const canvas = document.createElement("canvas");
+            canvas.width = this.width;
+            canvas.height = this.height;
+            this.context = canvas.getContext("2d");
+        },
+
+        render: function () {
+            const duration = 1000;
+            const t = (performance.now() % duration) / duration;
+
+            const radius = (pulseSize / 2) * 0.3;
+            const outerRadius = (pulseSize / 2) * 0.7 * t + radius;
+            const context = this.context;
+
+            // Draw the outer circle.
+            context.clearRect(0, 0, this.width, this.height);
+            context.beginPath();
+            context.arc(
+                this.width / 2,
+                this.height / 2,
+                outerRadius,
+                0,
+                Math.PI * 2,
+            );
+            context.fillStyle = `rgba(255,239,0, ${1 - t})`;
+            context.fill();
+
+            // Update this image's data with data from the canvas.
+            this.data = context.getImageData(
+                0,
+                0,
+                this.width,
+                this.height,
+            ).data;
+
+            // map.triggerRepaint();
+
+            // image was updated
+            return true;
+        },
+    };
 </script>
 
 <MapLibre
@@ -69,6 +121,7 @@
 >
     <NavigationControl />
     <ScaleControl />
+    <Image id="pulsing-dot" image={pulsingDot} options={{ pixelRatio: 2 }} />
     {#if trackpoints}
         <GeoJSONSource data={trackpoints}>
             <CircleLayer
@@ -115,6 +168,17 @@
             data={`${PUBLIC_BASE_URL}/positions?${positionsSelector}`}
             bind:source={positions_source}
         >
+            <SymbolLayer
+                layout={{
+                    "icon-image": [
+                        "case",
+                        [">", ["get", "speed"], 0],
+                        "pulsing-dot",
+                        "",
+                    ],
+                    "icon-allow-overlap": true,
+                }}
+            />
             <CircleLayer
                 paint={{
                     "circle-color": [
